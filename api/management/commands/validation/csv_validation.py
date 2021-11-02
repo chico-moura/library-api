9af49cecd929965f2cce5634df9bab0fff518802
chico <chico.zimcolor@gmail.com>
2021-11-02 20:55:48 -0300

    CSVValidationService.validate_extension created

--- api/management/commands/validation/csv_validation.py
+++ api/management/commands/validation/csv_validation.py
@@ -1,4 +1,11 @@
+from api.exceptions.managment_commands_exceptions.file_does_not_have_csv_extension_exception import \
+    FileDoesNotHaveCSVExtensionException
+from api.management.management_enum import ManagementEnum
+
+
 class CSVValidationService:
     @staticmethod
-    def has_extension(file_name: str) -> None:
-        pass
+    def validate_extension(file_name: str) -> None:
+        extension = file_name.split('.')[-1]
+        if extension != ManagementEnum.CSV_EXTENSION:
+            raise FileDoesNotHaveCSVExtensionException()
---
+++ api/management/management_enum.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class ManagementEnum(Enum):
+    CSV_EXTENSION = 'csv'
