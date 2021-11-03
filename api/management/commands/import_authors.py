1ac0fe7fa89f95788b8d2c83361129a34e0641eb
chico <chico.zimcolor@gmail.com>
2021-11-02 22:51:01 -0300

    import_authors implemented

--- api/management/commands/import_authors.py
+++ api/management/commands/import_authors.py
@@ -1,21 +1,35 @@
-from enum import Enum
+import argparse
 import csv
 from django.core.management import BaseCommand, CommandParser
 
-
-class CommandEnum(Enum):
-    AUTHORS_CSV_FILE = 'authors_csv_file'
+from api.repositories.django_author_repository import DjangoAuthorRepository
+from domain.entities.author.dtos.author_dto import AuthorDTO
+from domain.services.save_author_service import SaveAuthorService
 
 
 class Command(BaseCommand):
+    __CSV_FILE: str = 'csv_file'
+    __SAVE_AUTHOR_SERVICE: SaveAuthorService
+
     help = 'Import authors from a CSV file to database.'
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        author_repository = DjangoAuthorRepository()
+        self.__SAVE_AUTHOR_SERVICE = SaveAuthorService(author_repository)
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
-            CommandEnum.AUTHORS_CSV_FILE.value,
+            self.__CSV_FILE,
+            type=argparse.FileType('r')
         )
 
-    def handle(self, *args, **options):
-        csv_file = options[CommandEnum.AUTHORS_CSV_FILE.value]
-        
-        self.stdout.write(csv_file)
+    def handle(self, *args, **options) -> None:
+        csv_file = options[self.__CSV_FILE]
+        author_reader = csv.reader(csv_file)
+
+        for row in author_reader:
+            author_name = ''.join(row)
+            author_dto = AuthorDTO(author_name)
+
+            self.__SAVE_AUTHOR_SERVICE.execute(author_dto)
