6c978c5b07b84d9a907e11d1c7e6469fce35f909
chico <chico.zimcolor@gmail.com>
2021-11-03 10:45:26 -0300

    ValueObject._validate is now a protected method

--- shared/value_object.py
+++ shared/value_object.py
@@ -3,8 +3,8 @@ from abc import ABC, abstractmethod
 
 class ValueObject(ABC):
     def __post_init__(self):
-        self.validate()
+        self._validate()
 
     @abstractmethod
-    def validate(self) -> None:
+    def _validate(self) -> None:
         pass
