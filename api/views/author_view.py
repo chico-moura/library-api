58abca1bf821c1e9e4bdc5a40ad3a3128ee18839
chico <chico.zimcolor@gmail.com>
2021-11-03 20:00:10 -0300

    author_dto_serializer created

--- api/views/author_view.py
+++ api/views/author_view.py
@@ -2,16 +2,15 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from domain.entities.author.dtos.author_dto import AuthorDTO
+from domain.repositories.author_repository import AuthorRepository
+from domain.services.save_author_service import SaveAuthorService
+from api.repositories.django_author_repository import DjangoAuthorRepository
 
 # TODO: -> JSON reponse
 # TODO: Integrated test mocking DB
 # TODO: Finish endpoint
 # TODO: CSV (django commands & pure python) (is CSV part of domain)
-
-from domain.entities.author.dtos.author_dto import AuthorDTO
-from domain.repositories.author_repository import AuthorRepository
-from domain.services.save_author_service import SaveAuthorService
-from api.repositories.django_author_repository import DjangoAuthorRepository
 
 
 class AuthorView(APIView):
@@ -20,6 +19,9 @@ class AuthorView(APIView):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.__author_repository = DjangoAuthorRepository()
+
+    def get(self) -> Response:
+        pass
 
     def post(self, request: Request, *args, **kwargs) -> Response:
         save_author_service = SaveAuthorService(self.__author_repository)
