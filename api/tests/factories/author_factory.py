f33ab76eb36c1c8b739245327ed0d718cc0eb402
chico <chico.zimcolor@gmail.com>
2021-11-05 16:41:28 -0300

    author_factory for tests created

---
+++ api/tests/factories/author_factory.py
@@ -0,0 +1,11 @@
+from domain.entities.author.author import Author
+from domain.entities.author.value_objects.author_id import AuthorId
+from domain.entities.author.value_objects.author_name import AuthorName
+
+
+class AuthorFactory:
+    @staticmethod
+    def new(name: str) -> Author:
+        author_name = AuthorName(name)
+        author_id = AuthorId()
+        return Author(name=author_name, id=author_id)
