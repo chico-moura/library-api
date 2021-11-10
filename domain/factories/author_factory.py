189606f08cdf92828ab6e0a3aa9b4dc839c149da
chico <chico.zimcolor@gmail.com>
2021-11-10 09:16:37 -0300

    implemented author factory

--- domain/factories/author_factory.py
+++ domain/factories/author_factory.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from domain.entities import Author
 from domain.entities.author.value_objects import AuthorId, AuthorName
 
@@ -7,5 +9,12 @@ class AuthorFactory:
     def build_new(name: str) -> Author:
         return Author(
             id=AuthorId(),
+            name=AuthorName(name)
+        )
+
+    @staticmethod
+    def build_existing(id_: UUID, name: str) -> Author:
+        return Author(
+            id=AuthorId(id_),
             name=AuthorName(name)
         )
